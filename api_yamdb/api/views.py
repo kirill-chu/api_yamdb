9c4cceae03996c47847906f380efae81442ab46c
Ilya Seleznev <blizardsis@yandex.ru>
2023-02-22 11:35:12 +0300

    create users and signup links

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,8 +1,9 @@
-from rest_framework import filters, mixins, viewsets
-from reviews.models import Category, Genre, Title
+from rest_framework import filters, generics, mixins, status, viewsets
+from rest_framework.response import Response
+from reviews.models import Category, Genre, Title, User
 
 from .permissions import AdminOrReadOnly
-from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
+from .serializers import CategorySerializer, GenreSerializer, TitleSerializer, UserSerializer
 
 
 class CreateDestroyListViewSet(mixins.CreateModelMixin,
@@ -10,6 +11,16 @@ class CreateDestroyListViewSet(mixins.CreateModelMixin,
                                mixins.ListModelMixin,
                                viewsets.GenericViewSet):
     pass
+
+
+class SignUp(generics.CreateAPIView):
+
+    def post(self, request):
+        username = request.data["username"]
+        email = request.data["email"]
+        if User.objects.filter(username=username).filter(email=email):
+            return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class CategoryViewSet(CreateDestroyListViewSet):
@@ -32,3 +43,10 @@ class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
     #permission_classes = (AdminOrReadOnly,) раскоммитить после создания админа
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('username',)
