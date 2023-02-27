3e233782634aa0122e95b50b5849640b36c5a74c
Kirill Chu <kirill@churakov.su>
2023-02-27 15:10:03 +0300

    bug fix

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from users.views import NewTokenView, SignUp, UserViewSet
 
 from .views import (CategoryViewSet, GenreViewSet, TitleViewSet,
-                    UserViewSet, ReviewViewSet, CommentViewSet)
+                    UserViewSet, ReviewViewSet)
 
 router_v1 = routers.DefaultRouter()
 router_v1.register('categories', CategoryViewSet)
@@ -12,9 +12,6 @@ router_v1.register('titles', TitleViewSet)
 router_v1.register('users', UserViewSet)
 router_v1.register(r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet,
                    basename='reviews')
-router_v1.register(
-    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
-    CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
