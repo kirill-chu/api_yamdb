00567b88e2a422a892cfc39e0bf1efe2a7adeddb
Ilya Selenzev <blizardsis@yandex.ru>
2023-02-28 16:31:58 +0300

    1

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -5,6 +5,24 @@ from rest_framework import serializers
 User = get_user_model()
 
 
+class SignUpSerializer(serializers.ModelSerializer):
+
+    def validate_username(self, value):
+        if value.lower() == 'me':
+            raise serializers.ValidationError(
+                f'Использование имени {value} '
+                f'в качестве username запрещено.'
+            )
+        return value
+
+    class Meta:
+        fields = (
+            'username',
+            'email',
+        )
+        model = User
+
+
 class UserSerializer(serializers.ModelSerializer):
     """User serializer for Users App."""
 
--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,48 +1,58 @@
 """Views for User App."""
+import random
+import string
+
 from django.core.mail import send_mail
 from rest_framework import filters, generics, permissions, status, viewsets
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, SignUpSerializer
 
 
 class SignUp(generics.CreateAPIView):
     """Class for retrive conconfirmation_code."""
 
     permission_classes = (permissions.AllowAny,)
+    serializer_class = SignUpSerializer
+
+    def get_code(self):
+        letters = string.ascii_lowercase
+        return ''.join(random.choice(letters) for i in range(16))
+
+    def send_code(self, code, email):
+        send_mail(
+            'code',
+            f'confirmation_code = {code}',
+            'admin@yamdb.ru',
+            [email],
+            fail_silently=False,
+        )
 
     def post(self, request):
         """GET confirmation_code."""
-        if "username" not in request.data:
-            response = {
-                "field_not_found": "username"
-            }
-            return Response(response, status=status.HTTP_400_BAD_REQUEST)
-        if "email" not in request.data:
-            response = {
-                "field_not_found": "email"
-            }
-            return Response(response, status=status.HTTP_400_BAD_REQUEST)
-        username = request.data["username"]
-        email = request.data["email"]
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        username = serializer.validated_data.get('username')
+        email = serializer.validated_data.get('email')
         if User.objects.filter(username=username, email=email).exists():
-            code = User.objects.filter(
-                username=username, email=email
-            ).get().confirmation_code
-            send_mail(
-                'code',
-                f'confirmation_code = {code}',
-                'admin@yamdb.ru',
-                [email],
-                fail_silently=False,
-            )
+            user = User.objects.filter(
+                username=username,
+                email=email
+            ).get()
+            if user.confirmation_code is not None:
+                code = self.get_code()
+                user.confirmation_code = code
+                user.save()
+            self.send_code(user.confirmation_code, user.email)
             return Response(request.data, status=status.HTTP_200_OK)
-        response = {
-            "error": "user not found",
-        }
-        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        user, created = User.objects.get_or_create(
+            username=username, email=email, confirmation_code=self.get_code()
+        )
+        user.save()
+        self.send_code(user.confirmation_code, user.email)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
 
 class NewTokenView(generics.CreateAPIView):
