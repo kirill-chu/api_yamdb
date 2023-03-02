c245fb48a7962def6aec02d18383874cd98e9bab
Ilya Seleznev <blizardsis@yandex.ru>
2023-03-02 17:13:47 +0300

    1 permissions

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -2,7 +2,6 @@
 from django.contrib.auth import get_user_model
 from django.core.validators import RegexValidator
 from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
 
 User = get_user_model()
 
