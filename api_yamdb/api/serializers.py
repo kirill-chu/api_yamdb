150cec270fd20ad618f9ce92fb20cf15214be526
Ilya Selenzev <blizardsis@yandex.ru>
2023-03-05 14:44:49 +0300

    serializers

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -133,3 +133,78 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+
+class SignUpSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        max_length=150,
+        validators=[regexp_validator],
+    )
+    email = serializers.EmailField(
+        max_length=254,
+    )
+
+    def validate_username(self, value):
+        if value.lower() == 'me':
+            raise serializers.ValidationError(
+                f'Использование имени {value} '
+                f'в качестве username запрещено.'
+            )
+        return value
+
+    class Meta:
+        fields = (
+            'username',
+            'email',
+        )
+        model = User
+
+
+class NewTokenSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = (
+            'username',
+            'confirmation_code',
+        )
+        model = User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """User serializer for Users App."""
+
+    class Meta:
+        fields = (
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'bio',
+            'role',
+        )
+        model = User
+
+
+class MeSerializer(serializers.ModelSerializer):
+    """User serializer for Users App."""
+
+    username = serializers.CharField(
+        max_length=150,
+        validators=[regexp_validator],
+        required=False
+    )
+    email = serializers.EmailField(
+        max_length=254,
+        required=False
+    )
+
+    class Meta:
+        fields = (
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'bio',
+            'role',
+        )
+        model = User
+        read_only_fields = ('role',)
